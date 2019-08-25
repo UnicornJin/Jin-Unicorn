21d2bb7939df8c6f517e38f9f3645675256fdbca
UnicornJin_windows <1072217974@qq.com>
2019-08-25 21:29:41 +0800

    CS1101s

--- Playground.java
+++ Playground.java
@@ -1,24 +1,34 @@
 public class Playground{
     public static void main(String[] args){
-        find a=new find();
-        System.out.print(a.count_charge(100));
+        gcd a=new gcd();
+        System.out.println(a.findgcd(206,40));
     }
 }
 
-class find{
-    public int count_charge(int amount){
-        return cc(amount,5);
+class expt{
+    public int get(int b,int n){
+        return this.fast_get(b,n,1);
     }
-    public int cc(int amount, int kinds){
-        return amount==0?1
-                        :amount<0||kinds==0?0
-                                           :this.cc(amount,kinds-1)+this.cc(amount-this.first(kinds),kinds);
+    int fast_get(int b,int n,int a){
+        return n==0?a
+                   :n%2==1?this.fast_get(b,n-1,a*b)
+                          :this.fast_get(b*b,n/2,a);
     }
-    public int first(int kinds){
-        return  kinds==1?1:
-                kinds==2?5:
-                kinds==3?10:
-                kinds==4?25:
-                kinds==5?50:0;
+}
+
+class times{
+    public int multi(int a,int b){
+        return fast_times(a,b,0);
+    }
+    int fast_times(int a,int b,int product){
+        return b==0?product
+                   :b%2==1?fast_times(a,b-1,product+a)
+                          :fast_times(2*a,b/2,product);
+    }
+}
+
+class gcd{
+    public int findgcd(int n,int m){
+        return m==0?n:this.findgcd(m,n%m);
     }
 }
